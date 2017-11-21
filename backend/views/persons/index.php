269bd27f41484c4ade0c6f5178b04623096300df
Tariq Tawalbeh <t.tawalbeh@etqantech.com>
2017-11-21 14:30:53 +0200

    adding migrations

--- backend/views/persons/index.php
+++ backend/views/persons/index.php
@@ -2,7 +2,6 @@
 
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
-/* @var $model \frontend\models\ContactForm */
 
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
@@ -11,7 +10,6 @@ use kartik\date\DatePicker;
 use yii\helpers\Url;
 
 $this->title = 'Personal Page';
-//$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-contact">
     <h1><?= Html::encode($this->title) ?></h1>
